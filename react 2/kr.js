94f4c88b858bf0556fe7af328f7ec7340b57308a
JADA JEEVAN PRAKASH <Jadajeevanprakash2002@gmail.com>
2025-04-07 18:27:45 +0530

    Add files via upload

---
+++ react 2/kr.js
@@ -0,0 +1,7 @@
+import {a,name,sayhello,students} from "./rect.js"
+import jeevan from "./rect.js"
+console.log(a)
+console.log(name(90,67))
+console.log(sayhello())
+console.log(students.name )
+console.log(jeevan)
---
+++ react 2/react.js
@@ -0,0 +1,20 @@
+const a =80
+const b =90
+export {a,b}
+
+export function name (a,b) {
+    return a-b
+}
+name()
+const sayhello=function(){
+    return "hello"
+}
+export{sayhello}
+let students = {
+    "name":"yuvuraj",
+    "rollno":"123",
+}
+export{students}
+
+let yuvaraj =12345678
+export default yuvaraj
